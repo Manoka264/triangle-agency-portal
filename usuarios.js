fc6558ac1b7a3b113b9c697cd77964ca6452fc47
Manoka264 <154303123+Manoka264@users.noreply.github.com>
2026-09-06 20:06:22 -0400

    Create usuarios.js

---
+++ usuarios.js
@@ -0,0 +1,15 @@
+// BANCO DE DADOS DE FUNCIONÁRIOS DA TRIANGLE AGENCY
+const AGENTES_AUTORIZADOS = {
+  "AG-7042": {
+    passcode: "coleta2026",
+    nome: "Agente Teste",
+    clearance: 1,
+    cargo: "Coletor de Campo Jr."
+  },
+  "SUP-0001": {
+    passcode: "delta!triangle9",
+    nome: "Supervisor Vance",
+    clearance: 3,
+    cargo: "Supervisor Regional"
+  }
+};
